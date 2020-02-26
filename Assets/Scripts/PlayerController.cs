8470522ef8f7581fbde2ffcda418ef72529a2fd3
wbschultz <59841909+wbschultz@users.noreply.github.com>
2020-02-26 08:02:38 -0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DeckRunner.Positioning;
+
+public class PlayerController : MonoBehaviour
+{
+    private VerticalTrack currentTrack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTrack = TrackList.centerTrack;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for now, move on a and d, later move cards
+        if (Input.GetMouseButtonDown(1))
+        {
+            MovePlayer(currentTrack.right);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MovePlayer(currentTrack.left);
+        }
+    }
+
+    /// <summary>
+    /// move the player left
+    /// </summary>
+    /// <param name="target">VerticalTrack object to jump to</param>
+    void MovePlayer(VerticalTrack target)
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(target.marker.x, transform.position.y, transform.position.z);
+            currentTrack = target;
+        }
+    }
+}
---
+++ Assets/Scripts/TrackList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeckRunner.Positioning
+{
+    public class TrackList
+    {
+        public static readonly VerticalTrack leftTrack;
+        public static readonly VerticalTrack centerTrack;
+        public static readonly VerticalTrack rightTrack;
+
+        // static constructor for three tracks
+        static TrackList()
+        {
+            // fill the information for the track list
+            leftTrack = new VerticalTrack();
+            centerTrack = new VerticalTrack();
+            rightTrack = new VerticalTrack();
+
+            leftTrack.marker = Camera.main.ViewportToWorldPoint(new Vector3(0.25f, 0f, 0f));
+            leftTrack.left = null;
+            leftTrack.right = centerTrack;
+
+            centerTrack.marker = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0f, 0f));
+            centerTrack.left = leftTrack;
+            centerTrack.right = rightTrack;
+
+            rightTrack.marker = Camera.main.ViewportToWorldPoint(new Vector3(0.75f, 0f, 0f));
+            rightTrack.left = centerTrack;
+            rightTrack.right = null;
+        }
+
+    }
+}
